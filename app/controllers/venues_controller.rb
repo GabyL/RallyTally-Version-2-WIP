2114c4e1ccac13c61f24caab3f490d93ac52ba46
3mily <emrcraig@gmail.com>
2015-02-06 20:27:53 +0000

    start venues erb

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,2 +1,23 @@
 class VenuesController < ApplicationController
+
+  def index
+    render :venues
+  end
+
+
+  def create
+    @venue = Venue.new(params)
+    if @venue.save
+      redirect_to user_event_guests_path
+    else
+      render :new
+    end
+  end
+
+  def new
+    @venue = Venue.new
+  end
+
 end
+
+
